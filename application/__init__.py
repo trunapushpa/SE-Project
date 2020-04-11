dc1024cc8fd9e7a213c4392bf4a25c1f8fbfb955
Harish <harishkaushal30@gmail.com>
2020-04-11 14:52:00 +0530

    added DB config

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from config import Config, ProductionConfig, DevelopmentConfig, TestingConfig
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+
+db = SQLAlchemy(app)
 
 from application import routes
