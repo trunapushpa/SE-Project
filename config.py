9bc0c2c75b218a9321f5c6b8aad0cb4df1277dbc
Harish <harishkaushal30@gmail.com>
2020-04-11 21:26:27 +0530

    Login and Registeration

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or b'1\xdc\x92N\xc9+n\xbf\x81<\xd7t\xca3\xe7b'
     DEBUG = False
     TESTING = False
     DBPWD = 'lostnfound@12345'
@@ -8,6 +9,7 @@ class Config(object):
     DBNAME = 'lostnfound'
     DBSERVER = 'lostnfounddb.postgres.database.azure.com:5432'
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DBUSER}:{DBPWD}@{DBSERVER}/{DBNAME}" 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     #"dbname='lostnfound' user='myadmin@lostnfounddb' host='lostnfounddb.postgres.database.azure.com' password='lostnfound@12345' port='5432' sslmode='true'"
 
 class ProductionConfig(Config):
