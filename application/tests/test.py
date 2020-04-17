811940fc17aef3c7da574c7b31e27be8a3e1eba0
Ganesh Vanahalli <g274707@gmail.com>
2020-04-17 10:03:32 +0530

    User Registration tests

--- application/tests/test.py
+++ application/tests/test.py
@@ -1,3 +1,5 @@
+import random
+import string
 import unittest
 
 import config
@@ -39,36 +41,33 @@ class ProjectTests(unittest.TestCase):
     #     self.assertIn(b'Some more catchy content goes here', response.data)
 
     def register(self, first_name, last_name, email, password, confirmPassword):
-        print("here")
         return self.app.post(
-            'register/',
+            '/register',
             data=dict(first_name=first_name, last_name=last_name, email=email, password=password,
                       confirmPassword=confirmPassword),
             follow_redirects=True
         )
 
-    # def test_user_registration_form_displays(self):
-    #     response = self.app.get('/register')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Sign up', response.data)
+    def test_user_registration_form_displays(self):
+        response = self.app.get('/register')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Sign up', response.data)
 
-    # def test_valid_user_registration(self):
-    #     self.app.get('/register', follow_redirects=True)
-    #     response = self.register('admin', 'admin', 'c@c.com', '12345678', '12345678')
-    #     print(response.status_code)
-    #     self.assertIn(b'You are successfully registered', response.data)
+    def test_valid_user_registration(self):
+        self.app.get('/register', follow_redirects=True)
+        random_email = ''.join(random.choices(string.ascii_lowercase, k=8)) + '@' + ''.join(
+            random.choices(string.ascii_lowercase, k=8)) + '.com'
+        response = self.register('admin', 'admin', random_email, '12345678', '12345678')
+        self.assertIn(b'You are successfully registered', response.data)
 
-    # def test_duplicate_email_user_registration_error(self):
-    #     self.app.get('/register', follow_redirects=True)
-    #     res = self.register('admin', 'admin', 'c@c.com', '12345678', '12345678')
-    #     self.app.get('/register', follow_redirects=True)
-    #     response = self.register('admin', 'admin', 'c@c.com', '12345678', '12345678')
-    #     print(res)
-    #     # self.assertIn(b'Email already registered, Please user another!!', response.data)
+    def test_duplicate_email_user_registration_error(self):
+        self.register('admin', 'admin', 'c@c.com', '12345678', '12345678')
+        response = self.register('admin', 'admin', 'c@c.com', '12345678', '12345678')
+        self.assertIn(b'Email already registered, Please user another!!', response.data)
 
-    # def test_user_profile_without_logging_in(self):
-    #     response = self.app.get('/userprofile')
-    #     self.assertEqual(response.status_code, 500)
+    def test_user_profile_without_logging_in(self):
+        response = self.app.get('/userprofile')
+        self.assertEqual(response.status_code, 500)
 
 
 if __name__ == "__main__":
