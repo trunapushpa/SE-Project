21b23d88dfd91afd355f75f0af1642ae55eafa2a
Trunapushpa <trunapushpa@gmail.com>
2020-04-25 01:21:40 +0530

    Moved get_item_description to uploadItemRoutes.py

--- application/routes/myItemsRoutes.py
+++ application/routes/myItemsRoutes.py
@@ -1,9 +1,8 @@
 import os
-from flask import Blueprint, redirect, flash, url_for, render_template, request, jsonify
+from flask import Blueprint, redirect, flash, url_for, render_template
 from flask_login import current_user, login_required
 from application import db, app
 from application.dbModels.items import Items
-from application.routes.indexRoutes import allowed_file
 
 my_items = Blueprint('my_items', __name__)
 
@@ -38,19 +37,3 @@ def change_item_state(item_id, state):
     db.session.commit()
     flash(f"Item marked {state}", 'success')
     return redirect(url_for('my_items.myitems'))
-
-
-# TODO: Fix twice image upload
-@my_items.route("/get_item_description", methods=['POST'])
-@login_required
-def get_item_description():
-    if 'file' not in request.files:
-        return 'File not sent in request', 406
-    file = request.files['file']
-    if file.filename == '':
-        return 'No file uploaded', 406
-    if file and allowed_file(file.filename):
-        # TODO: Send Real Description instead of file.filename
-        return jsonify(description=file.filename)
-    else:
-        return 'Allowed file types are png, jpg, jpeg', 415
--- application/routes/uploadItemRoutes.py
+++ application/routes/uploadItemRoutes.py
@@ -2,11 +2,9 @@ import os
 import time
 import uuid
 from datetime import datetime
-
-from flask import Blueprint, redirect, flash, url_for, render_template, request
+from flask import Blueprint, redirect, flash, url_for, render_template, request, jsonify
 from flask_login import current_user, login_required
 from werkzeug.utils import secure_filename
-
 from application import db, app
 from application.dbModels.items import Items
 from application.routes.indexRoutes import allowed_file, LOCATIONS
@@ -14,6 +12,22 @@ from application.routes.indexRoutes import allowed_file, LOCATIONS
 from ..ml.cv import extract_feature as image_extract_feature  
 
 upload_item = Blueprint('upload_item', __name__)
+
+
+# TODO: Fix twice image upload
+@upload_item.route("/get_item_description", methods=['POST'])
+@login_required
+def get_item_description():
+    if 'file' not in request.files:
+        return 'File not sent in request', 406
+    file = request.files['file']
+    if file.filename == '':
+        return 'No file uploaded', 406
+    if file and allowed_file(file.filename):
+        # TODO: Send Real Description instead of file.filename
+        return jsonify(description=file.filename)
+    else:
+        return 'Allowed file types are png, jpg, jpeg', 415
 
 
 @upload_item.route("/uploaditem", methods=['POST', 'GET'])
