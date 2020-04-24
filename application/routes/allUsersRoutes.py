17e1b44e3e335ef463f2d4237302012f87f688ff
Trunapushpa <trunapushpa@gmail.com>
2020-04-24 17:23:37 +0530

    UI: Nav, All Items, All Users

--- application/routes/allUsersRoutes.py
+++ application/routes/allUsersRoutes.py
@@ -5,11 +5,12 @@ from application.dbModels.users import Users
 
 all_users = Blueprint('all_users', __name__)
 
+
 @all_users.route('/allusers', methods=['POST', 'GET'])
 def allusers():
     if current_user.isadmin:
         # Logged in user should not be able to edit his/her account
-        users = Users.query.filter(Users.user_id != current_user.user_id).all()
+        users = Users.query.filter(Users.user_id != current_user.user_id).order_by(Users.user_id.asc()).all()
         return render_template("allusers.html", users = users)
     else:
         flash('Access denied to requested page', 'danger')
